89a87c6a867350b86279712aba23cbbda37e9980
Patrick Fitzgerald <auskast03@gmail.com>
2023-11-20 03:44:56 -0800

    use radix for radio group and tooltip

--- src/components/SocialPlatforms.tsx
+++ src/components/SocialPlatforms.tsx
@@ -2,52 +2,61 @@
 
 import * as React from "react";
 
+import * as RadioGroup from "@radix-ui/react-radio-group";
+import * as Tooltip from "@radix-ui/react-tooltip";
 import clsx from "clsx";
 
-import { RadioGroup, RadioGroupItem } from "./RadioGroup";
 import { FacebookIcon } from "./icons/FacebookIcon";
 import { LinkedinIcon } from "./icons/LinkedinIcon";
 import { RedditIcon } from "./icons/RedditIcon";
 import { TwitterIcon } from "./icons/TwitterIcon";
 
+const platforms: { name: string; value: string; icon: React.ReactNode }[] = [
+  {
+    name: "Facebook",
+    value: "facebook",
+    icon: <FacebookIcon />,
+  },
+  {
+    name: "X (formerly Twitter)",
+    value: "twitter",
+    icon: <TwitterIcon />,
+  },
+  {
+    name: "Reddit",
+    value: "reddit",
+    icon: <RedditIcon />,
+  },
+  {
+    name: "LinkedIn",
+    value: "linkedin",
+    icon: <LinkedinIcon />,
+  },
+];
+
 interface SocialPlatformsProps {}
 
 const SocialPlatforms = ({}: SocialPlatformsProps) => {
-  const [value, setValue] = React.useState<string>();
+  const [selected, setSelected] = React.useState<string>();
 
   return (
     <section className="mt-16">
       <h2 className="header2 mb-4">Social platform</h2>
-      <RadioGroup
+      <RadioGroup.Root
         className="flex gap-2"
         name="platform"
-        onValueChange={setValue}
+        onValueChange={setSelected}
       >
-        <SocialPlatform
-          name="Facebook"
-          value="facebook"
-          icon={<FacebookIcon />}
-          selected={value === "facebook"}
-        />
-        <SocialPlatform
-          name="X (formerly Twitter)"
-          value="twitter"
-          icon={<TwitterIcon />}
-          selected={value === "twitter"}
-        />
-        <SocialPlatform
-          name="Reddit"
-          value="reddit"
-          icon={<RedditIcon />}
-          selected={value === "reddit"}
-        />
-        <SocialPlatform
-          name="LinkedIn"
-          value="linkedin"
-          icon={<LinkedinIcon />}
-          selected={value === "linkedin"}
-        />
-      </RadioGroup>
+        {platforms.map(({ name, value, icon }) => (
+          <SocialPlatform
+            key={value}
+            name={name}
+            value={value}
+            icon={icon}
+            selected={selected === value}
+          />
+        ))}
+      </RadioGroup.Root>
     </section>
   );
 };
@@ -66,18 +75,33 @@ const SocialPlatform = ({
   selected,
 }: SocialPlatformProps) => {
   return (
-    <RadioGroupItem
-      className={clsx(
-        "border border-solid p-3 rounded-lg shadow-ai",
-        selected
-          ? "border-green bg-lightgreen/16 fill-green shadow-green/10"
-          : "border-gray bg-lightgray fill-black",
-      )}
-      value={value}
-      title={name}
-    >
-      {icon}
-    </RadioGroupItem>
+    <Tooltip.Provider>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild>
+          <RadioGroup.Item
+            className={clsx(
+              "border border-solid p-3 rounded-lg shadow-ai",
+              selected
+                ? "border-green bg-lightgreen/16 fill-green shadow-green/10"
+                : "border-gray bg-lightgray fill-black",
+            )}
+            value={value}
+          >
+            {icon}
+          </RadioGroup.Item>
+        </Tooltip.Trigger>
+        <Tooltip.Portal>
+          <Tooltip.Content
+            className="bg-black text-white text-xs leading-normal body2 p-1 rounded-md transition-opacity"
+            side="bottom"
+            sideOffset={4}
+          >
+            <Tooltip.Arrow />
+            {name}
+          </Tooltip.Content>
+        </Tooltip.Portal>
+      </Tooltip.Root>
+    </Tooltip.Provider>
   );
 };
 
